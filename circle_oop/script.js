4a1ffcbe8a90a2e1662416c9c85b20ee4991b968
YaroslavU <yarik.julman@gmail.com>
2022-09-05 13:52:42 +0300

    training

---
+++ circle_oop/script.js
@@ -0,0 +1,25 @@
+class Circle {
+    #r;
+    constructor(cx, cy, r) {
+        this.cx = cx;
+        this.cy = cy;
+        this.#r = r;
+    }
+    static create(cx, cy, r) {
+        const c = new Circle(cx, cy, r);
+        return c;
+    }
+    static square(r) {
+        var s = Math.PI * Math.pow(r, 2);
+        return s;
+    }
+    static length(r) {
+        var l = Math.PI * 2 * r;
+        return l;
+    }
+}
+
+var a = new Circle(5, 5, 8);
+var c = Circle.create(5, 5, 8);
+console.log(Circle.square(10));
+console.log(Circle.length(10));
